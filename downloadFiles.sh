3e84d7becf51aed85926006f7e5b10c704049ce8
Usama Hussain <uhussain@wisc.edu>
2019-05-05 23:13:44 +0200

    WJets18 file

--- downloadFiles.sh
+++ downloadFiles.sh
@@ -1,3 +1,4 @@
-xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2/90000/0C16FF8C-E081-E811-B7E4-7845C4FB6238.root
 xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/80000/F4498C6D-4FD6-E611-B470-02163E01444C.root
+xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2/20000/006010E1-C94F-E811-8521-44A842CFCA00.root
+xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/90000/FD68CE7E-9A28-B249-85B4-A5A8C3C4ADAF.root
 
