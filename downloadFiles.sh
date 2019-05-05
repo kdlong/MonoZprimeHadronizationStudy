b340f37a5723cfc087cfd96f5bea063348e8956c
Usama Hussain <uhussain@wisc.edu>
2019-05-05 22:08:48 +0200

    WJets files

---
+++ downloadFiles.sh
@@ -0,0 +1,3 @@
+xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2/90000/0C16FF8C-E081-E811-B7E4-7845C4FB6238.root
+xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/80000/F4498C6D-4FD6-E611-B470-02163E01444C.root
+
